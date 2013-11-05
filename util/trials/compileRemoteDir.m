676c7020ff2aec418f1fc03fe690270806ba3559
Balaji Sriram <balajisriram@gmail.com>
2013-11-04 20:26:28 -0800

    compileRemoteDir should include Box 6

--- util/trials/compileRemoteDir.m
+++ util/trials/compileRemoteDir.m
@@ -1,7 +1,7 @@
 function compileRemoteDir(RemoteDirBase,boxes,mice,recompile)
 
 if ~exist('boxes','var') || isempty(boxes)
-    boxes = {'Box1','Box2','Box3','Box4','Box5'};
+    boxes = {'Box1','Box2','Box3','Box4','Box5','Box6'};
 elseif ~iscell(boxes)
     error('either dont give boxes or give it as a sequence of cellstrs');
 end
