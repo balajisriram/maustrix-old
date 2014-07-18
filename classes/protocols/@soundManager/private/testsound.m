f65b8886c63b3ddb5ae41c10c7c604f79aa0f3c7
Balaji Sriram <balajisriram@gmail.com>
2014-07-17 17:40:59 -0700

    \old code did not have proper capitalization

--- classes/protocols/@soundManager/private/testsound.m
+++ classes/protocols/@soundManager/private/testsound.m
@@ -2,31 +2,31 @@ function testsound
 clear mex
 clear psychportaudio
 
-p=maxpriority('getsecs');
-priority(p);
-initializepsychsound(1);
+p=MaxPriority('getsecs');
+Priority(p);
+InitializePsychSound(1);
 try
-    x=psychportaudio('open',[],[],4,[],[],4096); %reqclass 4 doesn't work with asio4all + enhanced dll
+    x=PsychPortAudio('open',[],[],4,[],[],4096); %reqclass 4 doesn't work with asio4all + enhanced dll
 catch ex
     disp(['CAUGHT ERROR: ' getReport(ex,'extended')])
-    x=psychportaudio('open',[],[],2,[],[],4096);
+    x=PsychPortAudio('open',[],[],2,[],[],4096);
 end
-s=psychportaudio('getstatus',x);
+s=PsychPortAudio('getstatus',x);
 s.SampleRate
-psychportaudio('fillbuffer',x,rand(2,400000)-.5);
-getsecs;
+PsychPortAudio('fillbuffer',x,rand(2,400000)-.5);
+GetSecs;
 
 PsychPortAudio('RunMode', x, 1);
 
-psychportaudio('start',x);
-psychportaudio('stop',x);
+PsychPortAudio('start',x);
+PsychPortAudio('stop',x);
 
 val=true;
 for i=1:10
     val=~val;
-    y=getsecs;
-    psychportaudio('fillbuffer',x,val*rand(2,round(2*44100*i/10))-.5);
-    t=getsecs-y;
+    y=GetSecs;
+    PsychPortAudio('fillbuffer',x,val*rand(2,round(2*44100*i/10))-.5);
+    t=GetSecs-y;
     if val
         fprintf('yes:')
     else
@@ -34,50 +34,50 @@ for i=1:10
     end
     fprintf('\t%g\n',t)
 end
-y=getsecs;
-psychportaudio('start',x);
-psychportaudio('stop',x,2,0);
-t=getsecs-y;
+y=GetSecs;
+PsychPortAudio('start',x);
+PsychPortAudio('stop',x,2,0);
+t=GetSecs-y;
 fprintf('%g\n',t)
 pause
 for i=1:10
     waitForStop(x);
-    y=getsecs;
-    psychportaudio('start',x);
-    s=getsecs-y;
+    y=GetSecs;
+    PsychPortAudio('start',x);
+    s=GetSecs-y;
     pause(.5);
-    y=getsecs;
-    psychportaudio('stop',x,2,0);
-    t=getsecs-y;
+    y=GetSecs;
+    PsychPortAudio('stop',x,2,0);
+    t=GetSecs-y;
     fprintf('%g\t%g\n',s,t)
 end
 
 pause
 for i=1:10
     waitForStop(x);
-    y=getsecs;
-    psychportaudio('start',x);
-    s=getsecs-y;
+    y=GetSecs;
+    PsychPortAudio('start',x);
+    s=GetSecs-y;
     pause(.5);
-    y=getsecs;
-    psychportaudio('stop',x,0,0);
-    t=getsecs-y;
+    y=GetSecs;
+    PsychPortAudio('stop',x,0,0);
+    t=GetSecs-y;
     fprintf('%g\t%g\n',s,t)
 end
 
-psychportaudio('close')
+PsychPortAudio('close')
 
 x=audioplayer(rand(400000,2)-.5,44100);
 play(x);
 stop(x);
 for i=1:10
-    y=getsecs;
+    y=GetSecs;
     play(x);
-    s=getsecs-y;
+    s=GetSecs-y;
     pause(.5);
-    y=getsecs;
+    y=GetSecs;
     stop(x);
-    t=getsecs-y;
+    t=GetSecs-y;
     fprintf('%g\t%g\n',s,t)
 end
 priority(0);
--- util/infrastructure/waitForStop.m
+++ util/infrastructure/waitForStop.m
@@ -2,13 +2,13 @@ function waitForStop(x)
 
     startTime=GetSecs;
     numLoops=0;
-    s=psychportaudio('getstatus',x);
+    s=PsychPortAudio('getstatus',x);
 
     while s.Active
         numLoops = numLoops+1;
         %s
         % warning('when trying to start a sound, had to wait for psychportaudio to stop from previous sound -- check how recent last call to stop was')
-        s=psychportaudio('getstatus',x);
+        s=PsychPortAudio('getstatus',x);
     end
     time=GetSecs-startTime;
     if numLoops>1 || time>.0005
