e220fd6be97d7aa87ef1fdd6ca88d6e2b72ab44d
Balaji Sriram <balajisriram@gmail.com>
2014-07-23 11:44:02 -0700

    added final motion step to the protocol

--- setup/setupAG/mouseTraining_Motion_Lever.m
+++ setup/setupAG/mouseTraining_Motion_Lever.m
@@ -16,14 +16,14 @@ svnCheckMode='none';
 [fd_sto, fd_Center, fd_Sides, fd_Alt] = createFreeDrinksStepsAG_auto_Lever(svnRev,svnCheckMode);
 
 [ts_obj1, ts_obj2] = createObjectTrialSteps_auto(svnRev,svnCheckMode);
-% 
-% [ts_Moptim, ts_MvelSweep, ts_McohSweep, ts_Mdot1, ts_Mdot2, ts_Mdot3] = createMotionSteps_auto(svnRev,svnCheckMode);
+
+[ts_Moptim, ~, ~, ~, ~, ~] = createMotionSteps_auto(svnRev,svnCheckMode);
 
 
 %%%%%%%%%%% FINALLY make a protocol and put rats on it %%%%%%%%%%%%%%%%%
 
 % here is the protocol
 descriptiveString='mouseTraining_Motion_Lever';
-pMouseTraining_Motion_Lever = protocol(descriptiveString,{fd_sto,fd_Center,fd_Sides,fd_Alt,ts_obj1, ts_obj2});
+pMouseTraining_Motion_Lever = protocol(descriptiveString,{fd_sto,fd_Center,fd_Sides,fd_Alt,ts_obj1, ts_obj2, ts_Moptim});
 end
 
