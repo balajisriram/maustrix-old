3ebf1e74bb2bfe8bd3fba6a34fc149d782bd74e4
Balaji Sriram <balajisriram@gmail.com>
2014-07-29 22:56:12 -0700

    added box 6 and other experiment specific boxes

--- util/trials/compileRemoteDir.m
+++ util/trials/compileRemoteDir.m
@@ -1,13 +1,11 @@
 function compileRemoteDir(RemoteDirBase,boxes,mice,recompile)
-except = {'225','216','213','237','239'}
+except = {'213','216','225'}
 % 225 - 42603 - 42696 - box 1
-% 216 - 51397 - 51707 - box 2
-% 213 - 46099 - 46321 - box 3
-% 237 - 1419 - 1543 - box 4
-% 239 - 1168 - 1429 - box 5
+% 216 - 51394 - 51707 - box 2
+% 213 - 46098 - 46321 - box 3
 
 if ~exist('boxes','var') || isempty(boxes)
-    boxes = {'Box1','Box2','Box3','Box4','Box5','PV-V1-hM3D','PV-TRN-hM3D','SCLesion','RocheProject'};
+    boxes = {'Box1','Box2','Box3','Box4','Box5','Box6','PV-V1-hM3D','PV-TRN-hM3D','SCLesion','RocheProject','ORSweeps','VarDur','Motion','Reversal'};
 elseif ~iscell(boxes)
     error('either dont give boxes or give it as a sequence of cellstrs');
 end
