44ffe8d9de99d26e32ee20135897c4ab9d3a4ab1
Balaji Sriram <balajisriram@gmail.com>
2014-08-05 14:38:11 -0700

    226,221,220,238,240,213 dont have complete data

--- util/trials/compileRemoteDir.m
+++ util/trials/compileRemoteDir.m
@@ -1,11 +1,11 @@
 function compileRemoteDir(RemoteDirBase,boxes,mice,recompile)
-except = {'213','216','225'}
+except = {'213','216','225','226','221','220','238','240','213'}
 % 225 - 42603 - 42696 - box 1
 % 216 - 51394 - 51707 - box 2
 % 213 - 46098 - 46321 - box 3
 
 if ~exist('boxes','var') || isempty(boxes)
-    boxes = {'Box1','Box2','Box3','Box4','Box5','Box6','PV-V1-hM3D','PV-TRN-hM3D','SCLesion','RocheProject','ORSweeps','VarDur','Motion','Reversal'};
+    boxes = {'Box1','Box2','Box3','Box4','Box5','Box6','PV-V1-hM3D','PV-TRN-hM3D','SCLesion','ORSweeps','VarDur','Motion','Reversal'};
 elseif ~iscell(boxes)
     error('either dont give boxes or give it as a sequence of cellstrs');
 end
